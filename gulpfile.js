e7315c3b4000830f1555c07afa269ba564202af9
sergeykuzin <sergeykuzin2008@ya.ru>
2020-03-13 19:34:44 +0300

    build: added copy fonts

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,12 @@ gulp.task('copyImage', () => {
     .pipe(gulp.dest(dist + 'img/'));
 });
 
+gulp.task('copyFonts', () => {
+  return gulp
+    .src('./src/fonts/**/*')
+    .pipe(gulp.dest(dist + 'fonts/'));
+});
+
 gulp.task('clean', () => del('./dist'));
 
 gulp.task('build-js', () => {
@@ -88,7 +94,7 @@ gulp.task('watch', () => {
   gulp.watch('./src/img/**/*', gulp.parallel('copyImage'));
 });
 
-gulp.task('build', gulp.series('clean', 'buildHtml', 'buildCss', 'build-js', 'copyImage'));
+gulp.task('build', gulp.series('clean', 'buildHtml', 'buildCss', 'build-js', 'copyImage', 'copyFonts'));
 
 gulp.task('build-prod-js', () => {
   return gulp
