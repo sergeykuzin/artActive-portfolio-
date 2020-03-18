5516529eeed47c38c5b9c8b16e483c3dc99afa51
sergeykuzin <sergeykuzin2008@ya.ru>
2020-03-18 17:31:58 +0300

    feat: added 'feedback-from' section

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ReviewsSlider from './modules/ReviewsSlider';
 import MainMenuToggleButton from './modules/MainMenuToggleButton';
+import FeedbackForm from './modules/FeedbackForm';
 
 document.addEventListener('DOMContentLoaded', () => {
   const mainMenuToggleButton = new MainMenuToggleButton(
@@ -8,4 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const reviewsSlider = new ReviewsSlider('.reviews-slider');
   reviewsSlider.init();
+
+  const feedbackForm = new FeedbackForm('#feedback-form');
+  feedbackForm.init();
 });
